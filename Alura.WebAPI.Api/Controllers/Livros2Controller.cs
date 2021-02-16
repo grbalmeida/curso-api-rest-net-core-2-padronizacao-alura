bef0a387a71a0d24d2305a3dc29a4cd36b9928e0
gilvan <g.r.almeida@live.com>
2021-02-15 21:16:11 -0300

    Ordenando a coleção

--- Alura.WebAPI.Api/Controllers/Livros2Controller.cs
+++ Alura.WebAPI.Api/Controllers/Livros2Controller.cs
@@ -21,10 +21,11 @@ namespace Alura.WebAPI.Api.Controllers
         }
 
         [HttpGet]
-        public IActionResult ListaDeLivros([FromQuery] LivroFiltro filtro)
+        public IActionResult ListaDeLivros([FromQuery] LivroFiltro filtro, [FromQuery] LivroOrdem ordem)
         {
             var lista = _repo.All
                 .AplicaFiltro(filtro)
+                .AplicaOrdem(ordem)
                 .Select(l => l.ToApi())
                 .ToList();
 
---
+++ Alura.WebAPI.Api/Modelos/LivroOrdem.cs
@@ -0,0 +1,24 @@
+﻿using Alura.ListaLeitura.Modelos;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+namespace Alura.WebAPI.Api.Modelos
+{
+    public static class LivroOrdemExtensions
+    {
+        public static IQueryable<Livro> AplicaOrdem(this IQueryable<Livro> query, LivroOrdem ordem)
+        {
+            if (!string.IsNullOrEmpty(ordem?.OrdenarPor))
+            {
+                query = query.OrderBy(ordem.OrdenarPor);
+            }
+
+            return query;
+        }
+    }
+
+    public class LivroOrdem
+    {
+        public string OrdenarPor { get; set; }
+    }
+}
